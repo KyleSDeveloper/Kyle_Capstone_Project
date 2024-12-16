9758ac09ec6b3865c60c0ac392d61420a152ef72
Kyle <c0zychair@outlook.com>
2024-12-16 17:57:24 -0600

    cleaned up assets folder

--- assets/maps/platformerPack_industrial_vector.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="platformerPack_industrial_vector" tilewidth="70" tileheight="70" tilecount="170" columns="17">
- <image source="../../../Downloads/kenney_platformer-pack-industrial/Vector/platformerPack_industrial_vector.svg" width="1200" height="750"/>
-</tileset>

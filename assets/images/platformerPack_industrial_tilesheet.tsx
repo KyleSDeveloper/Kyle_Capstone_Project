96c8ebbf54a7c1e55338f12a797328a4121143be
Kyle <c0zychair@outlook.com>
2024-12-16 18:41:41 -0600

    made changes to level one and assets folder

--- assets/images/platformerPack_industrial_tilesheet.tsx
+++ assets/images/platformerPack_industrial_tilesheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="platformerPack_industrial_tilesheet" tilewidth="70" tileheight="70" tilecount="112" columns="14">
- <image source="../../../Downloads/kenney_platformer-pack-industrial/Tilesheet/platformerPack_industrial_tilesheet.png" width="980" height="560"/>
+ <image source="platformerPack_industrial_tilesheet.png" width="980" height="560"/>
 </tileset>
